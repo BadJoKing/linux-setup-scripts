664d2ed2c49ff72fdf206916afd08959cb030ddc
BadJoKing <100881959+BadJoKing@users.noreply.github.com>
2023-02-03 22:57:23 +0100

    force-yes

--- mint-setup.sh
+++ mint-setup.sh
@@ -1,18 +1,18 @@
 sudo apt-get update
 sudo apt update
-sudo apt full-upgrade
-sudo apt -y install *linux-firmware*
-sudo apt -y install steam
-sudo apt -y install lutris
-sudo apt -y install wine
-sudo apt -y install obs-studio
+sudo apt --yes --force-yes full-upgrade
+sudo apt --yes --force-yes install *linux-firmware*
+sudo apt --yes --force-yes install steam
+sudo apt --yes --force-yes install lutris
+sudo apt --yes --force-yes install wine
+sudo apt --yes --force-yes install obs-studio
 sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt install snapd
 sudo snap install discord
 sudo snap install libreoffice
 sudo snap install audacity
-sudo apt -y install virtualbox
+sudo apt --yes --force-yes install virtualbox
 sudo snap install blender --classic
 sudo snap install vlc
-sudo apt -y install python3.11*
+sudo apt --yes --force-yes install python3.11*
 
