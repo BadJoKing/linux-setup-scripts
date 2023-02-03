77bf005d71e605f5fba92cbc75a62310be9fb6f6
BadJoKing <100881959+BadJoKing@users.noreply.github.com>
2023-02-03 22:37:28 +0100

    Add files via upload

---
+++ mint-setup.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt update
+sudo apt full-upgrade
+sudo apt -y install *linux-firmware*
+sudo apt -y install steam
+sudo apt -y install lutris
+sudo apt -y install wine
+sudo apt -y install obs-studio
+sudo rm /etc/apt/preferences.d/nosnap.pref
+sudo apt install snapd
+sudo snap install discord
+sudo snap install libreoffice
+sudo snap install audacity
+sudo apt -y install virtualbox
+sudo snap install blender --classic
+sudo snap install vlc
+sudo apt -y install python3.11*
+
