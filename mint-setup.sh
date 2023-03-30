47be5284c97a98ec67ac4038b79c1639ccada47b
BadJoKing <100881959+BadJoKing@users.noreply.github.com>
2023-03-30 15:11:54 +0200

    Add files via upload

--- mint-setup.sh
+++ mint-setup.sh
@@ -1,22 +1,24 @@
-sudo apt-get update
 sudo apt update
+sudo apt full-upgrade
+sudo apt install linux-firmware
 sudo apt --yes --force-yes full-upgrade
 sudo apt --yes --force-yes install *linux-firmware*
 sudo apt --yes --force-yes install steam
 sudo apt --yes --force-yes install lutris
 sudo apt --yes --force-yes install wine
 sudo apt --yes --force-yes install obs-studio
+sudo apt --yes install linux-modules-6.1.0-1008-oem
 sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt install snapd
+sudo snap install eclipse --classic
 sudo snap install discord
+sudo snap install blender
+sudo apt install --yes gimp
 sudo snap install libreoffice
 sudo snap install audacity
 sudo apt --yes --force-yes install virtualbox
 sudo snap install blender --classic
 sudo snap install vlc
 sudo apt --yes --force-yes install python3.11*
-sudo apt install isenkram
-sudo isenkram-autoinstall-firmware
-sudo apt install linux-modules-iwlwifi-6*
-sudo apt install linux-wlan-ng-firmware
-sudo apt install bluez-firmware
+
+
