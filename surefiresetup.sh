a8384e9043fbdeec782fccae53a29b69aeb829b9
BadJoKing <100881959+BadJoKing@users.noreply.github.com>
2023-09-22 15:43:49 +0200

    Delete surefiresetup.sh

--- surefiresetup.sh
+++
@@ -1,7 +0,0 @@
-sudo -s
-sudo mkdir /sfsetup
-sudo cd /sfsetup
-sudo apt install git
-gh repo clone /BadJoKing/linux-setup-scripts
-cd linux-setup-scripts
-while IFS= read -r line; do sudo aptitude -y install "${line}"; done < aptlist.txt
