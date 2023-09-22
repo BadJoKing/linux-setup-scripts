4e6fbbe214efc297743a98d5965bcf3c615710cf
BadJoKing <100881959+BadJoKing@users.noreply.github.com>
2023-09-22 15:36:55 +0200

    Update setup.sh

--- mint/setup.sh
+++ mint/setup.sh
@@ -1,91 +1,95 @@
 #!/usr/bin/bash
-sudo apt update
-sudo apt --yes full-upgrade
-sudo apt --yes install *linux-firmware*
-sudo apt --yes install lutris
-sudo apt --yes install gparted
-sudo apt --yes install obs-studio
-sudo rm /etc/apt/preferences.d/nosnap.pref
-sudo apt -y install snapd
-sudo snap install steam
-sudo snap install discord
-sudo snap install libreoffice
-sudo snap install vlc
-sudo apt -y install openjdk-19-jdk
+echo "all log files are saved to ./setup.log"
+sudo apt update > setup.log
+echo "Doing stuff, please don't shut down"
+sudo apt --yes full-upgrade >> setup.log
+sudo apt --yes install *linux-firmware* >> setup.log
+sudo apt --yes install lutris >> setup.log
+sudo apt --yes install gparted >> setup.log
+sudo apt --yes install obs-studio >> setup.log
+sudo apt -y install steam >> setup.log
+sudo flatpak install -y --noninteractive com.discordapp.Discord >> setup.log
+sudo flatpak install -y --noninteractive org.libreoffice.LibreOffice >> setup.log
+sudo flatpak install -y --noninteractive org.videolan.VLC >> setup.log
+sudo apt -y install openjdk-19-jdk >> setup.log
+sudo flatpak install -y --noninteractive org.phoenicis.playonlinux >> setup.log
 sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-sudo apt update
-sudo apt -y install --install-recommends winehq-devel
-sudo apt -y install winetricks
-sudo apt -y install proton*
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key >> setup.log
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources >> setup.log
+sudo apt update >> setup.log
+sudo apt -y install --install-recommends winehq-devel >> setup.log
+sudo apt -y install winetricks >> setup.log
+sudo apt -y install protontricks >> setup.log
+sudo flatpak install -y --noninteractive net.davidotek.pupgui2 >> setup.log
 echo "Are you using a laptop?(Y/N)"
 read tlp
 if [[ "${tlp,,}" == "y" ]]; then
-	sudo add-apt-repository ppa:linrunner/tlp
-	sudo apt updates
-	sudo apt -y install tlp tlp-drw
-	sudo service tlp start
-	sudo snap install auto-cpufreq
-	sudo auto-cpufreq --install
-	sudo cp auto-cpufreq.conf /etc
-	sudo auto-cpufreq --config=/etc/auto-cpufreq.conf
+	echo "installing and configuring tlp"
+	sudo add-apt-repository -y ppa:linrunner/tlp >> setup.log
+	sudo apt update >> setup.log
+	sudo apt -y install tlp >> setup.log
+ 	sudo cp tlp.conf /etc/tlp.d >> setup.log
+	sudo tlp start >> setup.log
+ 	sudo systemctl enable tlp >> setup.log
 fi
 echo "Do you want to install any Image/Audio editing utilities?(Y/N)"
 read pro
-if [[ "${pro,,}" == "y" ]]; then
-	sudo apt install --yes gimp
-	sudo snap install blender --classic
-	sudo snap install audacity
+if [ "${pro,,}" == "y" ]; then
+	echo "installing gimp"
+	sudo apt install --yes gimp >> setup.log
+ 	echo "installing blender"
+	sudo flatpak install -y --noninteractive org.blender.Blender >> setup.log
+ 	echo "installing audacity"
+	sudo flatpak install -y --noninteractive org.audacityteam.Audacity >> setup.log
+ 	
 fi
 echo "Do you want any programming utilities?(Y/N)"
 read prog
-if [[ "${prog,,}" == "y" ]]; then
-	sudo apt --yes install virtualbox
-	sudo apt --yes install python3.11*
-	sudo apt install python-is-python3
-	sudo snap install eclipse --classic --candidate
-	sudo snap install code --classic
-	sudo apt -y install godot3
-	sudo apt -y install gcc
-	sudo apt -y install git
-	sudo apt -y install codeblocks
+if [ "${prog,,}" == "y" ]; then
+	echo "installing virtualbox, python, eclipse, vscodium, godot, gcc, git, codeblocks, rust and haskell"
+	sudo apt --yes install virtualbox >> setup.log
+	sudo apt --yes install python3.11* >> setup.log
+	sudo apt install python-is-python3 >> setup.log
+	sudo flatpak install -y --noninteractive org.eclipse.Java >> setup.log
+	sudo flatpak install -y --noninteractive com.vscodium.codium >> setup.log
+	sudo apt -y install godot3 >> setup.log
+	sudo apt -y install gcc >> setup.log
+	sudo apt -y install git >> setup.log
+	sudo apt -y install codeblocks >> setup.log
 	sudo curl https://sh.rustup.rs | sh
 	sudo curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 fi
 echo "are you me?(Y/N)"
 read nerd
 if [ "${nerd,,}" == "y" ]; then
-	sudo wget https://github.com/DraqueT/PolyGlot/releases/download/3.5.1/PolyGlot-Ins-Lin.deb
-	sudo dpkg -i PolyGlot-Ins-Lin.deb
-	sudo dpkg -i protonvpn-stable-release_1.0.3-2_all.deb
-	sudo apt update
-	sudo apt -y install protonvpn
-	sudo snap install the-powder-toy
-	sudo apt install -y polyglot
-	sudo apt install -y lmms
-	sudo apt install -y freeciv*
-	sudo apt install -y notepadqq
-	sudo apt install -y caneda
-	sudo apt install -y gnusim8085
-	sudo apt install -y micropolis
-	sudo apt install -y fontforge
-	sudo apt install -y freecad
-	sudo apt install -y kicad
+	echo "installing polyglot, some junk and mainline"
+	sudo wget https://github.com/DraqueT/PolyGlot/releases/download/3.5.1/PolyGlot-Ins-Lin.deb >> setup.log
+	sudo dpkg -i PolyGlot-Ins-Lin.deb >> setup.log
+	sudo dpkg -i protonvpn-stable-release_1.0.3-2_all.deb >> setup.log
+	sudo apt update >> setup.log
+	sudo apt -y install protonvpn >> setup.log
+	sudo flatpak install -y --noninteractive uk.co.powdertoy.tpt >> setup.log
+	sudo apt install -y polyglot >> setup.log
+	sudo apt install -y lmms >> setup.log
+	sudo apt install -y freeciv* >> setup.log
+	sudo apt install -y notepadqq >> setup.log
+	sudo apt install -y caneda >> setup.log
+	sudo apt install -y gnusim8085 >> setup.log
+	sudo apt install -y micropolis >> setup.log
+	sudo apt install -y fontforge >> setup.log
+	sudo apt install -y freecad >> setup.log
+	sudo apt install -y kicad >> setup.log
+ 	sudo wget https://github.com/lights0123/n-link/releases/download/v0.1.6/n-link_0.1.6_amd64.deb >> setup.log
+  	sudo dpkg -i n-link_0.1.6_amd64.deb >> setup.log
 	curl -L https://raw.githubusercontent.com/rust-lang/rustlings/main/install.sh | bash
-	sudo ln -s /lib/x86_64-linux-gnu/libssl.so.3 /lib/x86_64-linux-gnu/libssl.so.1.1
- 	sudo ln -s /lib/x86_64-linux-gnu/libcrypto.so.3 /lib/x86_64-linux-gnu/libcrypto.so.1.1
-  	sudo add-apt-repository ppa:cappelikan/ppa
-	sudo apt update
-	sudo apt -y install mainline 
-fi
+	sudo ln -s /lib/x86_64-linux-gnu/libssl.so.3 /lib/x86_64-linux-gnu/libssl.so.1.1 >> setup.log
+ 	sudo ln -s /lib/x86_64-linux-gnu/libcrypto.so.3 /lib/x86_64-linux-gnu/libcrypto.so.1.1 >> setup.log
+  	sudo add-apt-repository ppa:cappelikan/ppa >> setup.log
+	sudo apt update >> setup.log
+	sudo apt -y install mainline >> setup.log 
+fi 
 sudo mkdir ~/bin/
 sudo cp ./setup.sh ~/bin
-#sudo mv ~/setup-lm/sys-update.sh sys-update
-#sudo mv ~/setup-lm/setup.sh
-echo "sudo apt update" | sudo tee -a ~/bin/sys-update.sh
-echo "sudo apt upgrade" | sudo tee -a ~/bin/sys-update.sh
 sudo chmod +x ~/bin/setup.sh
-sudo chmod +x ~/bin/sys-update.sh
 echo "export PATH=$PATH:~/bin" >> ~/.bashrc
 sudo apt -y autoremove
