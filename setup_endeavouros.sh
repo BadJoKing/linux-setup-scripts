91a49ab0066d9dbaa968ce375340c4321deab18e
BadJoKing <100881959+BadJoKing@users.noreply.github.com>
2024-04-21 22:05:41 +0200

    Create setup_endeavouros.sh

---
+++ setup_endeavouros.sh
@@ -0,0 +1,3 @@
+yay -Syy paru
+curl https://raw.githubusercontent.com/BadJoKing/linux-setup-scripts/main/bashrc > $HOME/.bashrc
+paru -Syy --noconfirm steam wine wine-gecko wine-mono protontricks winetricks asusctl supergfxctl rog-control-center blueman qemu-full virt-manager kdeconnect mint-y-icons amdvlk vulkan-radeon amdgpu-vulkan-switcher-git mercury-browser-bin discord discord-update-skip-git
