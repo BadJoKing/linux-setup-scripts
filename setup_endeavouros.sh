d3d7a7bdf042ee28bbb19ce5880c59f470c77226
BadJoKing <100881959+BadJoKing@users.noreply.github.com>
2024-05-17 14:59:34 +0200

    Update setup_endeavouros.sh
    Add tor stuff

--- setup_endeavouros.sh
+++ setup_endeavouros.sh
@@ -6,5 +6,8 @@ paru -Syy --noconfirm steam wine wine-gecko wine-mono protontricks winetricks am
 paru -Syy --noconfirm asusctl supergfxctl rog-control-center 
 # QOL stuff
 paru -Syy --noconfirm blueman qemu-full virt-manager kdeconnect mint-y-icons mercury-browser-bin discord discord-update-skip-git noise-suppression-for-voice 
+# Other stuff
+paru -Syy --noconfirm tor torsocks tor-browser-bin
+# this disables the usb write cache
 echo "SUBSYSTEMS==\"usb\", SUBSYSTEM==\"block\", ENV{ID_FS_USAGE}==\"filesystem\", ENV{UDISKS_MOUNT_OPTIONS_DEFAULTS}+=\"sync\", ENV{UDISKS_MOUNT_OPTIONS_ALLOW}+=\"sync\"" | sudo tee /etc/udev/rules.d/99-udisks2-usb_mount.rules
 sudo udevadm control --reload-rules
