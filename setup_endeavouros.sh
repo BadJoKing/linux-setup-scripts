a4ccd0a5651bc9ec20b117441f4949368c4f02a2
BadJoKing <100881959+BadJoKing@users.noreply.github.com>
2024-05-17 14:54:53 +0200

    Update setup_endeavouros.sh
    add noise-suppression-for-voice

--- setup_endeavouros.sh
+++ setup_endeavouros.sh
@@ -1,5 +1,10 @@
-yay -Syy paru
+yay -Syy --noconfirm paru
 curl https://raw.githubusercontent.com/BadJoKing/linux-setup-scripts/main/bashrc > $HOME/.bashrc
-paru -Syy --noconfirm steam wine wine-gecko wine-mono protontricks winetricks asusctl supergfxctl rog-control-center blueman qemu-full virt-manager kdeconnect mint-y-icons amdvlk vulkan-radeon amdgpu-vulkan-switcher-git mercury-browser-bin discord discord-update-skip-git
+# Gaming stuff
+paru -Syy --noconfirm steam wine wine-gecko wine-mono protontricks winetricks amdvlk vulkan-radeon amdgpu-vulkan-switcher-git  
+# Asus stuff
+paru -Syy --noconfirm asusctl supergfxctl rog-control-center 
+# QOL stuff
+paru -Syy --noconfirm blueman qemu-full virt-manager kdeconnect mint-y-icons mercury-browser-bin discord discord-update-skip-git noise-suppression-for-voice 
 echo "SUBSYSTEMS==\"usb\", SUBSYSTEM==\"block\", ENV{ID_FS_USAGE}==\"filesystem\", ENV{UDISKS_MOUNT_OPTIONS_DEFAULTS}+=\"sync\", ENV{UDISKS_MOUNT_OPTIONS_ALLOW}+=\"sync\"" | sudo tee /etc/udev/rules.d/99-udisks2-usb_mount.rules
 sudo udevadm control --reload-rules
